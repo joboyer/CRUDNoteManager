a255b692c2a265ecff0e404e98bd7dd363e02c9b
Jordan Boyer <jordan@blueboard.io>
2021-04-09 13:42:19 +0200

    Fix Crud rules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,94 +9,35 @@ export default function App() {
   );
 }
 
-// import React, { Component } from "react";
-// import "./App.css";
+// Import React!
+// import React from 'react'
+// import { Editor } from 'slate-react'
+// import { Value } from 'slate'
 
-// class App extends Component {
-
+// class App extends React.Component {
 //   state = {
-//     notes: [],
-//     currentNote: "",
-//     noteEditing: null,
-//     currentEdit: ""
-//   };
-
-//   componentDidMount() {
-//     const json = localStorage.getItem("notes");
-//     const notes = JSON.parse(json);
-//     if (notes) {
-//       this.setState(() => ({ notes }));
-//     }
+//     value: initialValue,
 //   }
 
-//   componentDidUpdate(prevProps, prevState) {
-//     this.state.notes.forEach((note, index) => {
-//       if (prevState.notes[index] !== note) {
-//         const json = JSON.stringify(this.state.notes);
-//         localStorage.setItem("notes", json);
-//       }
-//     });
-//   }
-
-//   addNote = () => {
-//     let notes = [...this.state.notes];
-//     if (this.state.noteEditing != null && typeof this.state.noteEditing === 'number') {
-//       notes[this.state.noteEditing] = this.state.currentNote
-//       this.setState({ notes, currentNote: "", noteEditing: null });
-//       return
+//   onChange = ({ value }) => {
+//     if (value.document !== this.state.value.document) {
+//       const content = JSON.stringify(value.toJSON())
+//       localStorage.setItem('content', content)
 //     }
-//     notes.push(this.state.currentNote);
-//     this.setState({ notes, currentNote: "" });
-//   };
-
-//   deleteNote = indexToDelete => {
-//     let notes = [...this.state.notes].filter(
-//       (note, index) => index !== indexToDelete
-//     );
-//     this.setState({ notes });
-//   };
-
-//   setNoteEditing = index => {
-//     // this.setState({ noteEditing: index, currentEdit: this.state.notes[index] });
-//     this.setState({ noteEditing: index, currentNote: this.state.notes[index] });
-//   };
-
-//   editNote = event => {
-//     this.setState({ currentEdit: event.target.value });
-//   };
-
-//   submitEdit = index => {
-//     let notes = [...this.state.notes];
-//     notes[index] = this.state.currentEdit;
-//     this.setState({ notes, noteEditing: null });
-//   };
+//     this.setState({ value })
+//   }
 
 //   render() {
 //     return (
-//       <div className="App">
-//         <h1>
-//           <textarea
-//             onChange={event => this.setState({ currentNote: event.target.value })}
-//             value={this.state.currentNote}
-//             className="input"
-//             placeholder="Notes"
-//           />
-//           <br />
-//           <button className="button" onClick={this.addNote}>Submit</button>
-//         </h1>
-//         {this.state.notes.map((note, index) => (
-//           <div className="notes" key={index} onClick={() => this.setNoteEditing(index)}>
-//             <div className="note">
-//               <div className="note-content">
-//                 <div className="note-text">{note}</div>
-//               </div>
-//               <button onClick={() => this.deleteNote(index)}>Delete</button>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     );
+//       <Editor
+//         plugins={plugins}
+//         value={this.state.value}
+//         onChange={this.onChange}
+//         renderMark={this.renderMark}
+//       />
+//     )
 //   }
+
 // }
 
 // export default App;
--- src/components/NoteManager/index.js
+++ src/components/NoteManager/index.js
@@ -1,8 +1,57 @@
 
 import React, { Component } from "react";
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
 import NoteListItem from '../NoteListItem';
 import Toolbar from '../Toolbar';
 import './NoteManager.css';
+
+function MarkHotkey(options) {
+    const { type, key } = options
+
+    // Return our "plugin" object, containing the `onKeyDown` handler.
+    return {
+        onKeyDown(event, editor, next) {
+            // If it doesn't match our `key`, let other plugins handle it.
+            if (!event.ctrlKey || event.key !== key) return next()
+
+            // Prevent the default characters from being inserted.
+            event.preventDefault()
+
+            // Toggle the mark `type`.
+            editor.toggleMark(type)
+        },
+    }
+}
+
+// Initialize a plugin for each mark...
+const plugins = [
+    MarkHotkey({ key: 'b', type: 'bold' }),
+    MarkHotkey({ key: '`', type: 'code' }),
+    MarkHotkey({ key: 'i', type: 'italic' }),
+    MarkHotkey({ key: '~', type: 'strikethrough' }),
+    MarkHotkey({ key: 'u', type: 'underline' }),
+]
+
+const existingValue = JSON.parse(localStorage.getItem('content'))
+const initialValue = Value.fromJSON(
+    existingValue || {
+        document: {
+            nodes: [
+                {
+                    object: 'block',
+                    type: 'paragraph',
+                    nodes: [
+                        {
+                            object: 'text',
+                            text: 'A line of text in a paragraph.',
+                        },
+                    ],
+                },
+            ],
+        },
+    }
+)
 
 class Messenger extends Component {
 
@@ -10,14 +59,25 @@ class Messenger extends Component {
         notes: [],
         currentNote: "",
         noteEditing: null,
-        currentEdit: ""
+        currentEdit: "",
+        value: initialValue,
+        valueArray: [],
     }
+    // state = {
+    //     value: initialValue,
+    // }
 
     componentDidMount = () => {
         const json = localStorage.getItem("notes");
+        const valueArray = localStorage.getItem("valueArray");
         const notes = JSON.parse(json);
+
         if (notes) {
             this.setState({ notes });
+        }
+
+        if (valueArray) {
+            this.setState({ valueArray });
         }
     }
 
@@ -26,6 +86,8 @@ class Messenger extends Component {
             if (prevState.notes[index] !== note && note !== "") {
                 const json = JSON.stringify(this.state.notes);
                 localStorage.setItem("notes", json);
+                const valueArray = JSON.stringify(this.state.valueArray);
+                localStorage.setItem("valueArray", valueArray);
             }
         });
     }
@@ -38,37 +100,46 @@ class Messenger extends Component {
     };
 
     setNoteEditing = index => {
+        console.log(this.state.valueArray[index])
         // this.setState({ noteEditing: index, currentEdit: this.state.notes[index] });
-        this.setState({ noteEditing: index, currentNote: this.state.notes[index] });
+        this.setState({ noteEditing: index, currentNote: this.state.notes[index], value: this.state.valueArray[index] });
     };
 
     editNote = event => {
         this.setState({ currentEdit: event.target.value });
     }
 
-    refreshUser = () => {
-        const json = localStorage.getItem("notes");
-        const notes = JSON.parse(json);
-        if (notes) {
-            this.setState({ notes });
-        }
-    }
-
     addNote = async () => {
         let notes = [...this.state.notes];
+        let valueArray = [...this.state.valueArray];
+
         if (this.state.currentNote !== "") {
             if (this.state.noteEditing != null && typeof this.state.noteEditing === 'number') {
                 notes[this.state.noteEditing] = this.state.currentNote
-                await this.setState({ notes: notes, currentNote: "", noteEditing: null });
+                valueArray[this.state.noteEditing] = this.state.value
+                await this.setState({ notes: notes, valueArray: valueArray, currentNote: "", noteEditing: null });
                 return
             }
             await notes.push(this.state.currentNote);
-            await this.setState({ notes: notes, currentNote: "" });
+            await valueArray.push(this.state.value);
+            await this.setState({ notes: notes, valueArray: valueArray, currentNote: "" });
         }
-    };
+    }
 
-    onChange = () => {
-        this.refreshUser()
+    onChange = ({ value }) => {
+        if (value.document !== this.state.value.document) {
+            const content = JSON.stringify(value.toJSON())
+            let data = JSON.parse(content)
+            let line = ""
+            data.document.nodes.map((node, index) => {
+                if ((node.nodes[0].text && node.nodes[0].text != "") && line == "") {
+                    line = node.nodes[0].text;
+
+                }
+            })
+            this.setState({ value, currentNote: line })
+            localStorage.setItem('content', content)
+        }
     }
 
     render() {
@@ -83,7 +154,7 @@ class Messenger extends Component {
                             this.state.notes.map((note, index) => (
                                 <div className="note-list-item" key={index} onClick={() => this.setNoteEditing(index)}>
                                     <button className="note-photo" onClick={() => this.deleteNote(index)}>
-                                        <i class="ti-close"></i>
+                                        <i className="ti-close"></i>
                                     </button>
                                     < NoteListItem
                                         key={index}
@@ -98,12 +169,18 @@ class Messenger extends Component {
                     <Toolbar
                         title="Edit Note"
                     />
-                    <div className="note-edit-container">
+                    <div className="note-edit-container" onChange={event => this.setState({ currentNote: event.target.value })}>
                         <h1>
-                            <textarea
+                            {/* <textarea
                                 onChange={event => this.setState({ currentNote: event.target.value })}
                                 value={this.state.currentNote}
                                 placeholder="Notes"
+                            /> */}
+                            <Editor
+                                plugins={plugins}
+                                value={this.state.value}
+                                onChange={this.onChange}
+                                renderMark={this.renderMark}
                             />
                             <br />
                             <button className="button" onClick={this.addNote}>Submit</button>
@@ -112,6 +189,24 @@ class Messenger extends Component {
                 </div>
             </div>
         )
+    }
+
+    renderMark = (props, editor, next) => {
+        switch (props.mark.type) {
+            case 'bold':
+                return <strong>{props.children}</strong>
+            // Add our new mark renderers...
+            case 'code':
+                return <code>{props.children}</code>
+            case 'italic':
+                return <em>{props.children}</em>
+            case 'strikethrough':
+                return <del>{props.children}</del>
+            case 'underline':
+                return <u>{props.children}</u>
+            default:
+                return next()
+        }
     }
 }
 
